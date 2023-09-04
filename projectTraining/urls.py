afdf0299026f08e68d0b611c04a4a6012924cbf8
Pantoja <jvpantojamaciel@gmail.com>
2023-09-04 15:51:32 -0300

    upgate ROUTES

--- projectTraining/urls.py
+++ projectTraining/urls.py
@@ -1,24 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def my_View(request):
-    return HttpResponse('HOME')
-
-
-def about(request):
-    return HttpResponse('about')
-
-
-def contact(request):
-    return HttpResponse('contact')
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_View),
-    path('sobre/', about),
-    path('contato/', contact),
+    path('', include('recipes.urls')),  # Incluindo as urls de recipes.
 ]
-""" Teste """
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def about(request):
+    return HttpResponse('about')
+
+
+def contact(request):
+    return HttpResponse('contact')
